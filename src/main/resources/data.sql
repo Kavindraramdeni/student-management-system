46527921b973c921b5d905009405900e67e8a339
Kavindraramdeni <kavindraraj1703@gmail.com>
2025-11-23 16:32:27 +0530

    Delete src/main/resources/data.sql

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO students (name, email, age, course)
-VALUES ('Asha Kumar', 'asha@example.com', 21, 'IT');
-
-INSERT INTO students (name, email, age, course)
-VALUES ('Ravi Patel', 'ravi@example.com', 22, 'Computer Science');
