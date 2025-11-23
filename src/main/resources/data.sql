c7305d8c77b36789db80a9afdb947bc092b6b954
Kavindraramdeni <kavindraraj1703@gmail.com>
2025-11-23 16:20:27 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO students (id, name, email, age, course) VALUES (1, 'Asha Kumar', 'asha@example.com', 21, 'IT');
-INSERT INTO students (id, name, email, age, course) VALUES (2, 'Ravi Patel', 'ravi@example.com', 22, 'Computer Science');
+INSERT INTO students (name, email, age, course)
+VALUES ('Asha Kumar', 'asha@example.com', 21, 'IT');
+
+INSERT INTO students (name, email, age, course)
+VALUES ('Ravi Patel', 'ravi@example.com', 22, 'Computer Science');
