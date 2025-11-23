c5e658a683095bfe20923c985b58442a0803e3ef
Kavindraramdeni <kavindraraj1703@gmail.com>
2025-11-23 17:07:34 +0530

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO students (name, email, age, course)
+VALUES ('Asha Kumar', 'asha@example.com', 21, 'IT');
+
+INSERT INTO students (name, email, age, course)
+VALUES ('Ravi Patel', 'ravi@example.com', 22, 'Computer Science');
